986826bef7ffe7e080d0ae451001ac85c140a736
lukeemersonj-design <luke.emerson.j@gmail.com>
2026-04-03 07:21:33 -0400

    Refactor index.js to use CommonJS and add proxy

--- index.js
+++ index.js
@@ -1,23 +1,26 @@
-import express from "express";
-import fetch from "node-fetch";
-import httpProxy from "http-proxy";
+const fetch = require("node-fetch");
+const httpProxy = require("http-proxy");
 
-const app = express();
 const proxy = httpProxy.createProxyServer({});
 
-app.get("/", async (req, res) => {
-  try {
-    const response = await fetch("https://jsonplaceholder.typicode.com/todos/1");
-    const data = await response.json();
-    res.json(data);
-  } catch (err) {
-    res.status(500).json({ error: err.message });
+module.exports = async (req, res) => {
+  // Root route example
+  if (req.url === "/") {
+    try {
+      const response = await fetch("https://jsonplaceholder.typicode.com/todos/1");
+      const data = await response.json();
+      res.status(200).json(data);
+    } catch (err) {
+      res.status(500).json({ error: err.message });
+    }
+  } 
+  // Proxy route example
+  else if (req.url.startsWith("/proxy/")) {
+    const target = req.url.replace("/proxy/", "");
+    proxy.web(req, res, { target: `https://${target}`, changeOrigin: true });
+  } 
+  // 404 for anything else
+  else {
+    res.status(404).send("Not Found");
   }
-});
-
-app.all("/proxy/*", (req, res) => {
-  const target = req.url.replace("/proxy/", "");
-  proxy.web(req, res, { target: `https://${target}`, changeOrigin: true });
-});
-
-export default app;
+};
